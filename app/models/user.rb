3c91918bad81f8526827a4178cb642d64f229cf8
israelgarcia <israelgarcia018@gmail.com>
2012-07-27 15:59:27 +0200

    add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+  validates :email, presence: true , format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false } 
   has_many :trades
 end
